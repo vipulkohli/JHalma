a864fb4186066bbea72656e73f1da8589de4a6a7
Vipul Kohli <vkohli@smu.edu>
2014-11-13 00:48:22 -0600

    working on CollisionAnalyst

---
+++ CollisionPart.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)CollisionPart.java
+ *
+ *
+ * @author 
+ * @version 1.00 2014/11/12
+ */
+
+public class CollisionPart {
+        
+    /**
+     * Creates a new instance of <code>CollisionPart</code>.
+     */
+    public CollisionPart() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
